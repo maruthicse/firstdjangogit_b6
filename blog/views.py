bfc5641ab772d252ca4f225dd644e22de0784724
maruthi <maruthimcse06@gmail.com>
2018-11-26 21:19:41 +0530

    firstversion2

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ def post_list(request):
 	# pull data base 
 	# send to the template
 	#posts = Post.objects.filter(published_date__lte=timezone.now())
-	posts = Post.objects.all()
-	return render(request,'blog\post_list.html',{'posts':posts})
+	#posts = Post.objects.all()
+	return render(request,'blog\post_list.html',{})
 def post_details(request,pk):
 	post = get_object_or_404(Post,pk=pk)
 	return render(request,'blog\post_details.html',{'post':post})
